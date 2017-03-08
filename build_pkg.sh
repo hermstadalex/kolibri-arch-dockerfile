63dfb974a655b424aee2b46fcbfb612cef30606a
Alex Hermstad <alex@Alexs-MacBook-Pro.local>
2017-03-08 15:45:28 -0800

    Added optional test in build_pkg

--- build_pkg.sh
+++ build_pkg.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 su kol -c "cd /home/kol/pypi2pkgbuild && ./pypi2pkgbuild.py -d -n --pre file://$(readlink -f /in/*.whl)"
 cp -r /home/kol/pypi2pkgbuild/*kolibri*/*.xz /out/
+
+#echo "y" | pacman -U /home/kol/pypi2pkgbuild/*kolibri*/*.xz
